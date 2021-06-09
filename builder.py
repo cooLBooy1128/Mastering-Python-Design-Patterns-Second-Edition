149de8a7dd73ae2e7c8b078d8298b182ceadbf1d
‘lb’ <1012514235@qq.com>
2021-06-09 23:43:06 +0800

    Builder Pattern - creational pattern

---
+++ builder.py
@@ -0,0 +1,196 @@
+from enum import Enum
+import time
+
+
+# computer_builder
+class Computer:
+    def __init__(self, serial_number):
+        self.serial = serial_number
+        self.memory = None
+        self.hdd = None
+        self.gpu = None
+
+    def __str__(self):
+        info = (f'Memory: {self.memory} GB',
+                f'Hard Disk: {self.hdd} GB',
+                f'Graphics Card: {self.gpu}')
+        return '\n'.join(info)
+
+
+class ComputerBuilder:
+    def __init__(self):
+        self.computer = Computer('AG23385193')
+
+    def configure_memory(self, amount):
+        self.computer.memory = amount
+
+    def configure_hdd(self, amount):
+        self.computer.hdd = amount
+
+    def configure_gpu(self, gpu_model):
+        self.computer.gpu = gpu_model
+
+
+class HardwareEngineer:
+    def __init__(self):
+        self.builder = None
+
+    def construct_computer(self, memory, hdd, gpu):
+        self.builder = ComputerBuilder()
+        self.builder.configure_memory(memory)
+        self.builder.configure_hdd(hdd)
+        self.builder.configure_gpu(gpu)
+
+    @property
+    def computer(self):
+        return self.builder.computer
+
+
+def main1():
+    engineer = HardwareEngineer()
+    print(engineer.computer if engineer.builder else None)
+    engineer.construct_computer(500, 8, 'GeForce GTX 650 TI')
+    print()
+    print(engineer.computer)
+
+
+# pizza_builder
+PizzaProgress = Enum('PizzaProgress', 'queued preparation baking ready')
+PizzaDough = Enum('PizzaDough', 'thin thick')
+PizzaSauce = Enum('PizzaSauce', 'tomato creme_fraiche')
+PizzaTopping = Enum('PizzaTopping',
+                    'mozzarella double_mozzarella bacon ham mushrooms red_onion oregano')
+STEP_DELAY = 3  # in seconds for the sake of the example
+
+
+class Pizza:
+    def __init__(self, name):
+        self.name = name
+        self.dough = None
+        self.sauce = None
+        self.topping = []
+
+    def __str__(self):
+        return self.name
+
+    def prepare_dough(self, dough):
+        self.dough = dough
+        print(f'preparing the {self.dough.name} dough of your {self}...')
+        time.sleep(STEP_DELAY)
+        print(f'done with the {self.dough.name} dough')
+
+
+class MargaritaBuilder:
+    def __init__(self):
+        self.pizza = Pizza('margarita')
+        self.progress = PizzaProgress.queued
+        self.baking_time = 5  # in seconds for the sake of the example
+
+    def prepare_dough(self):
+        self.progress = PizzaProgress.preparation
+        self.pizza.prepare_dough(PizzaDough.thin)
+
+    def add_sauce(self):
+        print('adding the tomato sauce to your margarita...')
+        self.pizza.sauce = PizzaSauce.tomato
+        time.sleep(STEP_DELAY)
+        print('done with the tomato sauce')
+
+    def add_topping(self):
+        topping_desc = 'double mozzarella, oregano'
+        topping_items = (PizzaTopping.double_mozzarella, PizzaTopping.oregano)
+        print(f'adding the topping ({topping_desc}) to your margarita')
+        self.pizza.topping.append([t for t in topping_items])
+        time.sleep(STEP_DELAY)
+        print(f'done with the topping ({topping_desc})')
+
+    def bake(self):
+        self.progress = PizzaProgress.baking
+        print(f'baking your margarita for {self.baking_time} seconds')
+        time.sleep(self.baking_time)
+        self.progress = PizzaProgress.ready
+        print('your margarita is ready')
+
+
+class CreamyBaconBuilder:
+    def __init__(self):
+        self.pizza = Pizza('creamy bacon')
+        self.progress = PizzaProgress.queued
+        self.baking_time = 7  # in seconds for the sake of the example
+
+    def prepare_dough(self):
+        self.progress = PizzaProgress.preparation
+        self.pizza.prepare_dough(PizzaDough.thick)
+
+    def add_sauce(self):
+        print('adding the crème fraîche sauce to your creamy bacon')
+        self.pizza.sauce = PizzaSauce.creme_fraiche
+        time.sleep(STEP_DELAY)
+        print('done with the crème fraîche sauce')
+
+    def add_topping(self):
+        topping_desc = 'mozzarella, bacon, ham, mushrooms, red onion, oregano'
+        topping_items = (PizzaTopping.mozzarella,
+                         PizzaTopping.bacon,
+                         PizzaTopping.ham,
+                         PizzaTopping.mushrooms,
+                         PizzaTopping.red_onion,
+                         PizzaTopping.oregano)
+        print(f'adding the topping ({topping_desc}) to your creamy bacon')
+        self.pizza.topping.append([t for t in topping_items])
+        time.sleep(STEP_DELAY)
+        print(f'done with the topping ({topping_desc})')
+
+    def bake(self):
+        self.progress = PizzaProgress.baking
+        print(f'baking your creamy bacon for {self.baking_time} seconds')
+        time.sleep(self.baking_time)
+        self.progress = PizzaProgress.ready
+        print('your creamy bacon is ready')
+
+
+class Waiter:
+    def __init__(self):
+        self.builder = None
+
+    def construct_pizza(self, builder):
+        self.builder = builder
+        steps = (builder.prepare_dough,
+                 builder.add_sauce,
+                 builder.add_topping,
+                 builder.bake)
+        [step() for step in steps]
+
+    @property
+    def pizza(self):
+        return self.builder.pizza
+
+
+def validate_style(builders):
+    try:
+        input_msg = 'What pizza would you like, [m]argarita or [c]reamy bacon? '
+        pizza_style = input(input_msg)
+        builder = builders[pizza_style]()
+    except KeyError:
+        error_msg = 'Sorry, only margarita (key m) and creamy bacon (key c) are available'
+        print(error_msg)
+        return False, None
+    return True, builder
+
+
+def main2():
+    builders = dict(m=MargaritaBuilder, c=CreamyBaconBuilder)
+    valid_input = False
+    while not valid_input:
+        valid_input, builder = validate_style(builders)
+    print()
+    waiter = Waiter()
+    waiter.construct_pizza(builder)
+    pizza = waiter.pizza
+    print()
+    print(f'Enjoy your {pizza}!')
+
+
+if __name__ == '__main__':
+    # main1()
+    main2()
